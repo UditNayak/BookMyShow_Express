42c72a803b03918ac8cab084d64e093dee79de20
UditNayak <uditnayak027@gmail.com>
2024-07-10 11:11:07 +0530

    Implemented  bcrypt Hash Algorithm

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require('bcrypt')
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
    try {
+    const userExist = await User.findOne({ email: req.body.email });
+    if(userExist) return res({
+      message: "User already exists!",
+      status: 400
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    console.log(salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
     const newUser = new User(req.body);
     await newUser.save();
     res.status(201).json("User was registered successfully!");
